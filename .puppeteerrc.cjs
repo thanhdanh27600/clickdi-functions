e7f636ba3adc41e41a80bd078a45ff1e5fc67a7f
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-08-31 12:50:03 +0700

    use remote build

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-// .puppeteerrc.cjs
-const path = require("path");
-
-module.exports = {
-	cacheDirectory:
-		process.env.AZURE_FUNCTIONS_ENVIRONMENT === "Development"
-			? undefined
-			: path.join(__dirname, ".cache", "puppeteer"),
-};
